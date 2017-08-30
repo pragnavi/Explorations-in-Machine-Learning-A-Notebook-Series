34f9c4342217d4249e6e1cc62509349a56efdb6b
pragnavi <pragnavi1398@gmail.com>
2017-08-30 20:03:10 +0530

    Peer-Life, the Universe, and Everything

---
+++ TEST.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class TEST
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s = new Scanner(System.in);
+	    int i=0,p=0;
+        while(i == 0){
+            p = s.nextInt();
+            if(p==42)
+                break;
+            else
+                System.out.println(p);
+        }// your code goes here
+	}
+}
